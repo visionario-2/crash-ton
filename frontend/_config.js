7a019aecea0f3ff1d7352a533d0336ee510f1b84
visionario-2 <visionariod1git4l@gmail.com>
2025-10-23 11:14:44 -0300

    Fix formatting of CONFIG object in _config.js

--- frontend/_config.js
+++ frontend/_config.js
@@ -1,6 +1,6 @@
 <script>
-window.CONFIG = {
-  API_BASE: "https://crash-ton.onrender.com",
-  TON_MANIFEST_URL: "/public/tonconnect-manifest.json"
-};
+  window.CONFIG = {
+    API_BASE: "https://crash-ton.onrender.com",
+    TON_MANIFEST_URL: "/public/tonconnect-manifest.json"
+  };
 </script>
